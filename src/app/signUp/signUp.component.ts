083c9bcb839b8d7c39634fc98b2b2a0ab5ad0b32
tako <dokvadzetako@gmail.com>
2019-07-09 01:54:02 +0400

    Finish task-1

--- src/app/signUp/signUp.component.ts
+++ src/app/signUp/signUp.component.ts
@@ -1,62 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validator, FormControl, Validators } from '@angular/forms'
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-signUp',
     templateUrl: './signUp.component.html',
     styleUrls: ['./signUp.component.scss']
 })
-
 export class signUpComponent implements OnInit {
-    checkedForm
+    checkedform;
     constructor(
         private formBuilder: FormBuilder
-    ) {
-        this.checkedForm = formBuilder.group({
-            signUp: formBuilder.group({
-                email: new FormControl('', Validators.compose([
-                    Validators.required,
-                    Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
-                  ])),
-                password: ['',Validators.required],
-                confirmPassword: ['',Validators.required],
-                nickname: ['',Validators.required],
-                phoneNumber:['',Validators.required],
-                website: ['',Validators.required]
-            },{
-              //  validators: this.crossValidation
-            })
-        });
+        ){
+        this.checkedform = formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            passwordGroup: formBuilder.group({
+                password: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9 ._-]+$/), Validators.minLength(7)]],
+                confirmPassword: ['', Validators.required],
+            }, {
+                    validators: this.checkPasswords
+                }),
+            nickname: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9 ._-]+$/)]],
+            phoneNumber: ['', [Validators.required, Validators.pattern(/\+380[0-9]{9}$/)]],
+            website: ['',Validators.required, Validators.pattern(/(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)],
+            agreement: ['', Validators.required]
+        })
+    }
 
+    checkPasswords(formGroup) {
+        return formGroup.get('password').value === formGroup.get('confirmPassword').value ? null : { incorrect: true };
     };
 
-    ngOnInit() { };
 
-    onSubmit(value) {
-        console.log(value);
-        this.checkedForm.reset();
-    }
+    ngOnInit() {
+    };
 
-    // crossValidation(formGroup){
-    //     const email = formGroup.get('email').value;
-    //     const emailStatus = signUpComponent.isEmailOk(email);
+    get passwordGroup() {
+        return this.checkedform.get('passwordGroup') as FormGroup;
+    };
 
-    //     return emailStatus ? null : {
-    //         emailStatus
-    //     };
-    // }
 
-    // static isEmailOk(email){
-    //     return email.charAt(0) !== "a"
-    // }
+    get password() {
+        return this.checkedform.get('passwordGroup').get('password') as FormControl;
+    };
 
-    get email(){
-        return this.checkedForm.get('email') as FormControl;
-    }
+    get confirmPassword() {
+        return this.checkedform.get('passwordGroup').get('confirmPassword') as FormControl;
+    };
 
-    get password(){
-        return this.checkedForm.get('password') as FormControl;
-    }
+    get nickname() {
+        return this.checkedform.get('nickname') as FormControl;
+    };
 
+    get phoneNumber() {
+        return this.checkedform.get('phoneNumber') as FormControl;
+    };
+
+    get website() {
+        return this.checkedform.get('website') as FormControl;
+    };
+
+    get agreement() {
+        return this.checkedform.get('agreement') as FormControl;
+    };
+
+    get email() {
+        return this.checkedform.get('email') as FormControl;
+    };
 
 }
