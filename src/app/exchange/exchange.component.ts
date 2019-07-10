f43ad9e3c052560b4e54a9f4456af4ab2227438d
tako <dokvadzetako@gmail.com>
2019-07-11 03:27:04 +0400

    feat(Observable): In progress task-3

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CurrencyService } from './../currency.service'
-import { FormBuilder } from '@angular/forms';
+//import { FormBuilder } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 
 @Component({
@@ -14,13 +15,19 @@ export class ExchangeComponent implements OnInit {
   currencies;
   firstCurrencyValue: number;
   secondCurrencyValue: number;
+  thirdCurrencyValue: number;
   currency1;
   currency2;
+  currency3;
   Observer;
   rates;
   test1;
   test2;
-  constructor(private currencyService: CurrencyService,
+  form: FormGroup;
+  arrayForm: FormGroup;
+  constructor(
+    private currencyService: CurrencyService,
+    private fb: FormBuilder,
     private http: HttpClient,
     private formBuilder: FormBuilder) {
     this.currencies = currencyService.getcurrencies();
@@ -28,7 +35,12 @@ export class ExchangeComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    this.form = this.fb.group({
+      input: ''
+    });
+    this.arrayForm = this.fb.group({
+      input: this.fb.array([this.createInput()])
+    })
   }
 
   onChange(value) {
@@ -82,5 +94,45 @@ export class ExchangeComponent implements OnInit {
     const result2 = +Object.values(value.rates)
     this.firstCurrencyValue = this.secondCurrencyValue * result2;
   }
-}
 
+  // task-3
+
+  keyUp3(value) {
+    this.thirdCurrencyValue = value;
+   // console.log(value);
+  }
+
+  onChange3(value) {
+    this.currency3 = value;
+   // console.log(value);
+  }
+
+  get inputs() {
+    return (this.arrayForm.get('input') as FormArray).controls;
+  }
+
+  createInput() {
+    return this.fb.control('')
+  }
+
+  addCurrency() {
+    const control = this.arrayForm.get('input') as FormArray;
+    control.push(this.createInput());
+  }
+
+
+  sum() {
+    if (this.currency3) {
+      const url = ` https://api.exchangeratesapi.io/latest?base=${'USD'}&symbols=${this.currency3}`;
+      this.http.get(url).subscribe(value => {
+        this.updateThirdCurrencyValue(value);
+        //console.log(value);
+      });
+    }
+  }
+
+  updateThirdCurrencyValue(value) {
+    const result3 = +Object.values(value.rates);
+    this.thirdCurrencyValue = this.thirdCurrencyValue * result3;
+  }
+}
