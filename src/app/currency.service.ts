7a895b72cd8a951e06093ca72b67d33cf020a861
unknown <dokvadzetako@gmail.com>
2019-07-10 16:53:16 +0400

    feat(Observable Task-1): Finish Task-1

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { data } from './currencies';
 import { Observable, from } from 'rxjs'
 import { HttpClient } from '@angular/common/http';
-import { url } from 'inspector';
 
 @Injectable({
   providedIn: 'root'
@@ -36,5 +35,4 @@ export class CurrencyService {
       })
     };
   }
-
 }
--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CurrencyService } from './../currency.service'
+import { FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-exchange',
@@ -8,16 +12,51 @@ import { CurrencyService } from './../currency.service'
 })
 export class ExchangeComponent implements OnInit {
   currencies;
-  
-  constructor(private currencyService: CurrencyService ) { 
+  firstCurrencyValue: number;
+  secondCurrencyValue: number;
+  currency1;
+  currency2;
+  Observer;
+  rates;
+
+  constructor(private currencyService: CurrencyService,
+    private http: HttpClient,
+    private formBuilder: FormBuilder) {
     this.currencies = currencyService.getcurrencies();
+    this.Observer = new Observable(this.exchange);
   }
 
   ngOnInit() {
+    
   }
 
-  onChange(value){
+  onChange(value) {
+    this.currency1 = value;
     console.log(value)
   }
 
+  keyUp(value) {
+    this.firstCurrencyValue = value;
+  }
+
+  onChange2(value) {
+    this.currency2 = value;
+  }
+
+  exchange() {
+    console.log(this.currency1 + ' ' + this.firstCurrencyValue + ' ' + this.currency2);
+    if (this.currency1 && this.currency2) {
+      const url = ` https://api.exchangeratesapi.io/latest?base=${this.currency1}&symbols=${this.currency2}`;
+      this.http.get(url).subscribe(value => {
+        this.updateSecondCurrencyValue(value);
+      });
+    }
+  }
+
+  updateSecondCurrencyValue(value) {
+    const result = +Object.values(value.rates)
+    this.secondCurrencyValue = this.firstCurrencyValue*result;
+  }
+
 }
+
